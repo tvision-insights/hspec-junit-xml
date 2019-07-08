09ee725fb739531f6b059389f5adac44232dac62
Moss Prescott <moss@tvisioninsights.com>
2019-07-08 12:29:38 -0400

    Add a relatively complete example

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # hspec-junit-xml
 
 Formatter for Hspec that creates test reports in the "JUnit" format that's understood by CI tools,
-including the elasped time of each test. The elapsed time is also added to the Hspec's `specdoc`
+including the elasped time of each test. The elapsed time is also added to Hspec's `specdoc`
 formatter's output for human consumption.
 
 The time formatter can also be used separately.
+
+## Example
+
+A complete example of running a Spec can be found in [example/](example/test/Spec.hs). The output
+is [examples/test-results/examples.xml](examples/test-results/examples.xml).
---
+++ examples/test/Spec.hs
@@ -0,0 +1,38 @@
+import Control.Concurrent (threadDelay)
+import Test.Hspec (Spec, describe, example, it, shouldBe, shouldSatisfy)
+import Test.Hspec.Formatters.Xml (hspecWithElapsedTimes, hspecWithElapsedTimesAndReport)
+import Test.QuickCheck (property)
+
+main :: IO ()
+main = do
+  -- hspecWithElapsedTimes $
+  --   describe "with times" spec
+
+  hspecWithElapsedTimesAndReport "test-results" "examples" $
+    describe "with times and report" spec
+
+spec :: Spec
+spec = do
+  it "succeeds, quickly" $ do
+    threadDelay 2_000
+    pure ()
+
+  it "succeeds, not so fast" $ do
+    threadDelay 1_000_000
+    pure ()
+
+  it "fails" $ do
+    threadDelay 3_000
+    "an actual String" `shouldBe` "the String we said we expected"
+
+  it "errors" $ example $ do
+    threadDelay 4_000
+    fail "an exception in IO"
+
+  it "succeeds, with arbitrary input" $ property $ \ (x :: Int) -> do
+    threadDelay 100
+    x `shouldBe` x
+
+  it "fails, with arbitrary input" $ property $ \ (x :: Int) -> do
+    threadDelay 200
+    x `shouldSatisfy` (< 10)
